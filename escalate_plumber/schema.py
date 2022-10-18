f88bdd92d105a62abb4a6d41f74c2198e4ab1ec3
qai <qai1@fordham.edu>
2022-10-18 00:07:37 -0400

    add `__eq__` to `Material`

--- escalate_plumber/schema.py
+++ escalate_plumber/schema.py
@@ -37,6 +37,9 @@ class Material(MSONable):
     def __repr__(self):
         return self.name
 
+    def __eq__(self, other):
+        return self.inchikey == other.inchikey
+
     def __hash__(self):
         return hash(self.inchikey)
 
